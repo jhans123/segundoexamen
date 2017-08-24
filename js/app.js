844bc427348e93bba6b7af3c0cb81f331fdbf12e
jhans123 <jhans.tito@gmail.com>
2017-08-24 17:18:34 -0500

    Subiendo javascrip

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$('#example').popover(options)
+var x =5;
+var z=3;
+if(x>z){
+	var t = 'bienvenido';
+}else{
+	var t = 'no bienvenido';
+}
+var frutas =['platano','papaya','mango'];
+for (var i=0; i <frutas.length; i++) {
+	if (frutas[i]=='platano') {
+		console.log(frutas[i]);
+	}		
+}
+
+var numero=1;
+switch(numero){
+	case '1':
+		console.log('estoy en 1');
+		break;
+	case'2':
+		 console.log('estoy en 2');
+	 	break;
+	default:
+	  console.log('estoy en 3');
+}
+
